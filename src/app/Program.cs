44c039dc92c377143ee436f7b2266ecf16b104fa
Marytom Sousa <162215949+Marytomsousa@users.noreply.github.com>
2024-12-08 19:28:45 -0300

    Add files via upload

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -3,9 +3,20 @@ using app.Database;
 using app.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("PermitirTudo",
+            builder => builder
+                .AllowAnyOrigin() // Permite requisições de qualquer origem
+                .AllowAnyMethod() // Permite todos os métodos (GET, POST, etc.)
+                .AllowAnyHeader()); // Permite todos os cabeçalhos
+    });
+builder.Services.AddControllersWithViews()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EcoLogic")));
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
@@ -35,7 +46,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseCors("PermitirTudo");
 app.UseAuthentication();
 app.UseAuthorization();
 
